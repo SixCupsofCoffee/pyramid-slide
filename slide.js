bb4cee7889fe89043d478161918b1b2668801913
John Bell <ja.bell@me.com>
2017-03-13 10:57:15 -0400

    "initial commit"

---
+++ slide.js
@@ -0,0 +1,21 @@
+var heightElem = document.getElementById("height");
+var brickSymbol = document.getElementById("symbol");
+
+heightElem.oninput = function(event) {
+    // TODO: set the height based on the slider
+
+    // TODO: then draw or redraw the pyramid
+
+}
+
+brickSymbol.onchange = function(event) {
+    // TODO: change the symbol used to build the pyramid
+
+    // TODO: then draw or redraw the pyramid
+
+}
+
+function drawPyramid(height, symbol) {
+    // TODO: draw a pyramid of the height and with the symbol selected
+
+}
